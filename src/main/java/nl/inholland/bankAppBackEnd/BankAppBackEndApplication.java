5edae2f5107c3113157cf91bd765b2706e434de7
tnb1604 <damisavooges2549@gmail.com>
2025-04-16 10:44:39 +0200

    start.spring.io project generation

---
+++ src/main/java/nl/inholland/bankAppBackEnd/BankAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package nl.inholland.bankAppBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/inholland/bankAppBackEnd/BankAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.inholland.bankAppBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
