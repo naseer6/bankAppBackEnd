76be4193153210b73833937b3809ff019a3d4bd3
tnb1604 <damisavooges2549@gmail.com>
2025-05-07 14:10:17 +0200

    Revert "ffff"
    This reverts commit 03787a04b66c5e7982aba9b52139886ec9d6f5dc.

---
+++ sigmabankingapp/src/test/java/nl/inholland/sigmabankingapp/SigmabankingappApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.inholland.sigmabankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigmabankingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
