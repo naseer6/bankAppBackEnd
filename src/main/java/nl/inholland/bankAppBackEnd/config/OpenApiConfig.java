32b564cf7364520d2b819e897616e4bb77a7b61f
tnb1604 <damisavooges2549@gmail.com>
2025-06-01 15:52:06 +0200

    add openapiconfig class for authorize on swaggerui

---
+++ src/main/java/nl/inholland/bankAppBackEnd/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package nl.inholland.bankAppBackEnd.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Bank Application API")
+                        .description("API documentation for Sigma Banking Application")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Sigma Banking App")
+                                .url("https://github.com/naseer6/bankAppBackEnd")));
+    }
+}
